2160446479caf460ff3aeb5e0df11b3c06170a9f
Wang-Yi-Yu <2696835463@qq.com>
2026-03-27 20:20:12 +0800

    complete first experimental assignment

---
+++ experiment/experiment1/README.md
@@ -0,0 +1,15 @@
+<h1>COSC0025《并行程序设计》<br> 【CPU 架构编程】实验代码 </h1>
+
+## 实验 1：$n \times n$ 矩阵与向量乘积
+
+- [逐行访问算法](./dotProductByRow.cpp)
+- [逐列访问算法](./dotProductByColumn.cpp)
+  
+## 实验 2：$n$ 个数求和
+
+- [平凡算法](./sumTrivial.cpp)
+- [递归算法（递归函数形式）](./sumRecursive1.cpp)
+- [递归算法（循环形式）](./sumRecursive2.cpp)
+- [多链路算法（2路）](./sumMultiway-2.cpp)
+- [多链路算法（4路）](./sumMultiway-4.cpp)
+- [多链路算法（8路）](./sumMultiway-8.cpp)
--- experiment/experiment1/sumMultiway-2.cpp
+++ experiment/experiment1/sumMultiway-2.cpp
@@ -7,35 +7,34 @@ using namespace std;
 const int N = 1000000;
 const int IT = 500;
 
-long long a[N], b[N];
-
-long long sum() {
-    long long ans = 0; int len = N;
-    for (int i = 1;i <= (len + 1) / 2;i++)
-        b[i] = a[i * 2 - 1] + ((i * 2 > len) ? 0 : a[i * 2]);
-    len = (len + 1) / 2;
-    while (len > 0) {
-        if (len == 1) { ans = b[1]; break; }
-        else if (len == 2) { ans = b[1] + b[2]; break; }
-        for (int i = 1;i <= (len + 1) / 2;i++)
-            b[i] = b[i * 2 - 1] + ((i * 2 > len) ? 0 : b[i * 2]);
-        len = (len + 1) / 2;
-    }
-    return ans;
-}
-
+int a[N + 5];
 int main() {
     for (int i = 1;i <= N;i++)
-        a[i] = (long long)(i);
+        a[i] = i;
 
     long long startTime, endTime, Freq;
     QueryPerformanceFrequency((LARGE_INTEGER*)&Freq);
     QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
-    long long ans = 0;
-    for (int it = 1;it <= IT;it++)
-        ans = sum();
+
+    long long sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0, sum7 = 0, sum8 = 0;
+    long long sum = 0;
+    for (int it = 1;it <= IT;it++) {
+        sum1 = sum2 = sum3 = sum4 = sum5 = sum6 = sum7 = sum8 = sum = 0;
+        for (int i = 1;i <= N;i += 2) {
+            sum1 += a[i];
+            sum2 += a[i + 1];
+            // sum3 += a[i + 2];
+            // sum4 += a[i + 3];
+            // sum5 += a[i + 4];
+            // sum6 += a[i + 5];
+            // sum7 += a[i + 6];
+            // sum8 += a[i + 7];
+        }
+        sum = sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8;
+    }
+
     QueryPerformanceCounter((LARGE_INTEGER*)&endTime);
-    cout << "Sum: " << ans << endl;
+    cout << "Sum: " << sum << endl;
     cout << std::fixed << setprecision(2) << "Total Time: \t" << (double)(endTime - startTime) * 1000.0 / Freq << " ms" << endl;
     cout << std::fixed << setprecision(4) << "Avg Time:   \t" << (double)(endTime - startTime) * 1000.0 / (1.0 * IT * Freq) << " ms" << endl;
     return 0;
